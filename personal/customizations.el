92439bdefb677a8fde211bb703a5acf122a630b4
Dillon Kearns <dillon@dillonkearns.com>
2013-09-19 19:22:53 -0700

    Add rbenv

--- personal/customizations.el
+++ personal/customizations.el
@@ -3,3 +3,5 @@
 (setq ruby-insert-encoding-magic-comment nil)
 (global-set-key [remap move-beginning-of-line]
                 'move-beginning-of-line)
+
+(setq rbenv-show-active-ruby-in-modeline nil)
--- personal/init.el
+++ personal/init.el
@@ -1,3 +1,6 @@
 (server-start)   ;; start a server so we only have to open emacs once at startup
 
 (load-theme 'solarized-dark t)
+(require 'rbenv)
+(global-rbenv-mode)
+(setq rbenv-show-active-ruby-in-modeline nil)
