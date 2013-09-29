095f9b5d09af0e2ccfe5c78d3a8be7e437d68c9b
Dillon Kearns <dillon@dillonkearns.com>
2013-09-29 14:14:18 -0700

    Adjust some keybindings

--- personal/key-bindings.el
+++ personal/key-bindings.el
@@ -1,3 +1,4 @@
+;; super keys
 (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
 (global-set-key (kbd "s-w") 'er/expand-region)
 (global-set-key (kbd "s-W") 'er/contract-region)
@@ -5,17 +6,12 @@
 (global-set-key (kbd "s-g") 'ag-project)
 (global-set-key (kbd "s-G") 'ag-project-regexp)
 (global-set-key (kbd "s-T") 'dtk-tdd-split)
+
+
 (global-set-key (kbd "s-t") 'dtk/open-root-in-source-tree)
 (global-set-key (kbd "\C-cc") 'capture-work-note)
 (global-set-key (kbd "M-p") 'diff-hl-previous-hunk)
 (global-set-key (kbd "M-n") 'diff-hl-next-hunk)
 
-
 ;; key chords
-;; (key-chord-define-global "jb" 'ruby-beginning-of-block)
-;; (key-chord-define-global "je" 'ruby-end-of-block)
-(key-chord-define-global "JO" (lambda ()
-                                (interactive)
-                                (other-window -1))) ;; back one
-(key-chord-define-global "jo" 'other-window)
 (key-chord-define-global "jc" 'jump-to-corresponding)
