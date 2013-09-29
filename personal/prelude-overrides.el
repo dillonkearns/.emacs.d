e6dc308a829f18e81e7271e9a5233108a8f196f3
Dillon Kearns <dillon@dillonkearns.com>
2013-09-29 14:14:46 -0700

    Adjust some settings and remove duplicate global-linum-mode

--- personal/prelude-overrides.el
+++ personal/prelude-overrides.el
@@ -1,2 +1,4 @@
-(global-linum-mode t)
 (setq prelude-guru nil)
+
+(eval-after-load 'flycheck '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
+(setq projectile-switch-project-action 'projectile-dired)
