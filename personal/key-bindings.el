b9a83d664be32dfc8e073bfaf9af24942646622b
Dillon Kearns <dillon@dillonkearns.com>
2013-10-02 22:12:22 -0700

    Update some keybindings

--- personal/key-bindings.el
+++ personal/key-bindings.el
@@ -5,13 +5,16 @@
 (global-unset-key (kbd "s-g"))
 (global-set-key (kbd "s-g") 'ag-project)
 (global-set-key (kbd "s-G") 'ag-project-regexp)
+(global-set-key (kbd "s-S") 'dtk/open-root-in-source-tree)
+(global-set-key (kbd "s-t") 'projectile-find-test-file)
 (global-set-key (kbd "s-T") 'dtk-tdd-split)
 
 
-(global-set-key (kbd "s-t") 'dtk/open-root-in-source-tree)
 (global-set-key (kbd "\C-cc") 'capture-work-note)
 (global-set-key (kbd "M-p") 'diff-hl-previous-hunk)
 (global-set-key (kbd "M-n") 'diff-hl-next-hunk)
+(global-set-key (kbd "M-P") 'move-text-up)
+(global-set-key (kbd "M-N") 'move-text-down)
 
 ;; key chords
 (key-chord-define-global "jc" 'jump-to-corresponding)
