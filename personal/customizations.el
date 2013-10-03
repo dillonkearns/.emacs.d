8c37792d868f054f10ce18ca824a640468d8d6f5
Dillon Kearns <dillon@dillonkearns.com>
2013-10-03 15:18:17 -0700

    Make M-; comment current line when no active mark

--- personal/customizations.el
+++ personal/customizations.el
@@ -6,3 +6,21 @@
 
 (setq rbenv-show-active-ruby-in-modeline nil)
 (setq ace-jump-mode-scope 'window)
+
+
+;; Based on: http://programmers.stackexchange.com/a/19683
+;; Original idea from: http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at the end of the line,
+        then comment current line.
+        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (progn
+        (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+        (next-line)
+        )
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'comment-dwim-line)
