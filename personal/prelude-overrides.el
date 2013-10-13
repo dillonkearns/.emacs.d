12c5a48146a4ffe095ee4c6bd9938abda0525cc1
Dillon Kearns <dillon@dillonkearns.com>
2013-10-13 08:56:20 -0700

    Squelch whitespace warnings since it is so rampant it was getting annoying

--- personal/prelude-overrides.el
+++ personal/prelude-overrides.el
@@ -2,4 +2,4 @@
 
 (eval-after-load 'flycheck '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
 (setq projectile-switch-project-action 'projectile-dired)
-(setq whitespace-line-column 120)
+(setq whitespace-line-column 300)
