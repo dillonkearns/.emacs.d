ff6b4646bf987c70d7552d7a94bf2d56f226a90c
Dillon Kearns <dillon@dillonkearns.com>
2013-10-13 08:56:50 -0700

    Use diminish to clean up the modeline

--- personal/init.el
+++ personal/init.el
@@ -3,3 +3,8 @@
 (require 'rbenv)
 (global-rbenv-mode)
 (setq rbenv-show-active-ruby-in-modeline nil)
+
+(eval-after-load "ruby-test-mode" '(diminish 'ruby-test-mode))
+(eval-after-load "yas/minor-mode" '(diminish 'yas/minor-mode))
+(eval-after-load "Projectile" '(diminish 'projectile-mode))
+(eval-after-load "ruby-block" '(diminish 'ruby-block-mode))
