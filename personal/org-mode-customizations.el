0168fe095908092fd2a4a30cf63a600722c46ecb
Dillon Kearns <dillon@dillonkearns.com>
2013-11-13 07:43:45 -0800

    Add mailplane support

--- personal/org-mode-customizations.el
+++ personal/org-mode-customizations.el
@@ -17,3 +17,12 @@
 )
 
 (setq org-src-fontify-natively 't)
+
+;; mailplane support
+(org-add-link-type "mailplane" 'mailplane-open)
+
+(defun mailplane-open (path)
+  "Visit the manpage on PATH.
+     PATH should be a topic that can be thrown at the man command."
+  (message (format "open mailplane:%s"  path))
+  (shell-command (format "open mailplane:%s"  path)))
