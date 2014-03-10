68759da8a251d37f6ec3effa3d6f52a5e7290f2c
Dillon Kearns <dillon@dillonkearns.com>
2014-03-09 22:26:05 -0700

    Update god-mode config, and depend on the published package, not local compiled file.

--- personal/init/init-god-mode.el
+++ personal/init/init-god-mode.el
@@ -1,14 +1,22 @@
-(load-file "~/Dropbox/opensource/prelude/personal/god-mode.elc")
+;; (load-file "~/Dropbox/opensource/prelude/personal/god-mode.elc")
 (require 'god-mode)
 
 (defun my-update-cursor ()
-  (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+  (setq cursor-type
+        (cond
+         (god-local-mode 'box)
+         (buffer-read-only 'bar)
+         (t 'bar)
+              )))
 
 (add-hook 'god-mode-enabled-hook 'my-update-cursor)
 (add-hook 'god-mode-disabled-hook 'my-update-cursor)
+(add-hook 'after-change-major-mode-hook 'my-update-cursor)
 (god-mode)
 
 (global-set-key (kbd "<escape>") (lambda () (interactive) (god-local-mode 1)))
+(define-key god-local-mode-map (kbd "i") 'god-local-mode)
+(define-key god-local-mode-map (kbd "z") 'repeat)
 (key-chord-mode -1)
 
 (provide 'init-god-mode)
